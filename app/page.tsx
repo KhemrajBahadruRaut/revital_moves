b9035cdee7c3e02ff10d667bf3db0db05dc946c3
KhemrajBahadurRaut <amanraut76@gmail.com>
2026-04-27 16:46:10 +0545

    app updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Navbar from "@/components/header/navbar/Navbar.jsx"
+import Hero from "@/components/hero/Hero.jsx"
 export default function page() {
   return (
-    <div>
-      
-    </div>
+   <>
+   <Navbar/>
+   <Hero/>
+
+   </>
   )
 }
